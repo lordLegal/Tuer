df711f8087348a5e866407d97da544f05773313d
lordLegal <beneder.martin@gmail.com>
2020-11-11 10:31:27 +0100

    Create Tür.py

---
+++ Tür.py
@@ -0,0 +1,37 @@
+import msvcrt as m
+  
+    
+print("_________________________________________")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|                                   xx")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|   	                                 |")
+print("|________________________________________|")
+
+def wait():
+    print("Press any key to continue...") 
+    m.getch() 
+
+wait()
+
+
+
+
